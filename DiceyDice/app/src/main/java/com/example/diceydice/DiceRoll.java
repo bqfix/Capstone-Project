a34768da281856d50c1fd860b34d20ba8c35d5b2
Benjamin Fix <44587362+bqfix@users.noreply.github.com>
2019-05-28 11:05:49 -0400

    feat: Added a roll method to DiceRoll
    Currently only handles addition correctly.

--- DiceyDice/app/src/main/java/com/example/diceydice/DiceRoll.java
+++ DiceyDice/app/src/main/java/com/example/diceydice/DiceRoll.java
@@ -2,6 +2,8 @@ package com.example.diceydice;
 
 import android.support.v4.util.Pair;
 
+import java.util.ArrayList;
+
 public class DiceRoll {
 
     private String mName;
@@ -38,7 +40,32 @@ public class DiceRoll {
      * @return A pair, with the first value being a string containing each individual roll, and the second being the total.
      */
     public Pair<String, Integer> roll(){
-        //TODO Implement logic to roll Dice based off of the formula and return each roll, and the total.
-        return new Pair<>("1+1", 2);
+        //TODO Adjust to handle negative values correctly
+        StringBuilder compiledRolls = new StringBuilder("");
+        int total = 0;
+        ArrayList<String[]> splitFormulaByD =  new ArrayList<>();
+
+        String[] splitFormulaByPlusMinus = mFormula.trim().split("[+-]"); //Split formula based on + and -
+        for (String section : splitFormulaByPlusMinus) {
+            String trimmedSection = section.trim();
+            splitFormulaByD.add(trimmedSection.split("[dD]")); //Add each trimmed section to the ArrayList, splitting it by d or D if applicable
+        }
+
+        for (String[] splitRoll : splitFormulaByD) {
+            if (splitRoll.length == 3) { //If the size is 3, it was delineated by d or D, and thus we know that the first value is the numberOfDice, and the third value is the dieSize
+                int numberOfDice = Integer.parseInt(splitRoll[0]);
+                int dieSize = Integer.parseInt(splitRoll[2]);
+                Pair<String, Integer> rolledValues = Utils.calculateDice(numberOfDice, dieSize); //Use the utils method to calculate a Pair with the individual values, and the total, and append/total these
+                compiledRolls.append(rolledValues.first).append(" ");
+                total += rolledValues.second;
+            }
+
+            if (splitRoll.length == 1){ //If the length is one, simply append the number
+                String number = splitRoll[0];
+                compiledRolls.append("(").append(number).append(")");
+                total += Integer.parseInt(number);
+            }
+        }
+        return new Pair<>(compiledRolls.toString(), total);
     }
 }
