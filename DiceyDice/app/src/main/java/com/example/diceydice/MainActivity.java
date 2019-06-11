6ed832cfc9e4153193cbd5cf3ab7aba1ef763906
bqfix <benjaminfix@gmail.com>
2019-06-11 15:23:29 -0400

    feat: Added DKeyboard to MainActivity

--- DiceyDice/app/src/main/java/com/example/diceydice/MainActivity.java
+++ DiceyDice/app/src/main/java/com/example/diceydice/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.diceydice;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.util.Pair;
 import android.support.v7.app.AlertDialog;
@@ -10,10 +11,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.InputType;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputConnection;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
@@ -32,6 +36,8 @@ public class MainActivity extends AppCompatActivity implements FavoriteDiceRollA
     private FavoriteDiceRollAdapter mFavoriteDiceRollAdapter;
     private Button mAllFavoritesButton;
     private ImageButton mHelpButton;
+    private DKeyboard mDKeyboard;
+    private InputConnection mCommandInputConnection;
 
 
     @Override
@@ -41,9 +47,11 @@ public class MainActivity extends AppCompatActivity implements FavoriteDiceRollA
 
         assignViews();
 
-        setListeners();
+        setOnClickListeners();
 
         setupFavoriteRecyclerView();
+
+        setupEditTextAndKeyboard();
 
     }
 
@@ -65,6 +73,7 @@ public class MainActivity extends AppCompatActivity implements FavoriteDiceRollA
         mFavoriteRecyclerView = findViewById(R.id.main_favorite_rv);
         mAllFavoritesButton = findViewById(R.id.favorites_button);
         mHelpButton = findViewById(R.id.main_help_button);
+        mDKeyboard = (DKeyboard) findViewById(R.id.d_keyboard);
     }
 
     /**
@@ -79,13 +88,23 @@ public class MainActivity extends AppCompatActivity implements FavoriteDiceRollA
     }
 
     /**
-     * A helper method to hide the soft keyboard
+     * A helper method to hide the system keyboard
      *
      * @param view used to get the window token (passed in from listener)
      */
-    private void hideKeyboard(View view) {
+    private void hideSystemKeyboard(View view) {
         InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
         imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+    }
+
+    /** A helper method to show the custom keyboard */
+    private void showCustomKeyboard() { //TODO Add animation to make appear/disappear less jarring
+        mDKeyboard.setVisibility(View.VISIBLE);
+    }
+
+    /** A helper method to hide the custom keyboard */
+    private void hideCustomKeyboard() {
+        mDKeyboard.setVisibility(View.GONE);
     }
 
     /**
@@ -115,23 +134,14 @@ public class MainActivity extends AppCompatActivity implements FavoriteDiceRollA
     }
 
     /**
-     * Helper method to set listeners to various views, should only be called once in onCreate
+     * Helper method to set click listeners to various views, should only be called once in onCreate
      */
-    private void setListeners() {
+    private void setOnClickListeners() {
         mAllFavoritesButton.setOnClickListener(new View.OnClickListener() { //Click listener to launch FavoritesActivity
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, FavoriteActivity.class);
                 startActivity(intent);
-            }
-        });
-
-        mCommandInputEditText.setOnFocusChangeListener(new View.OnFocusChangeListener() { //FocusChange listener to minimize keyboard when clicking outside of EditText
-            @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                if (!hasFocus) {
-                    hideKeyboard(v);
-                }
             }
         });
 
@@ -144,7 +154,8 @@ public class MainActivity extends AppCompatActivity implements FavoriteDiceRollA
                     DiceRoll diceRoll = new DiceRoll(formula);
                     DiceResults diceResults = diceRoll.roll(MainActivity.this);
                     setDataToResultsViews(diceResults);
-                    hideKeyboard(v);
+                    hideCustomKeyboard();
+                    hideSystemKeyboard(v);
                 } else {
                     Toast.makeText(MainActivity.this, validAndErrorPair.second, Toast.LENGTH_SHORT).show();
                 }
@@ -194,6 +205,35 @@ public class MainActivity extends AppCompatActivity implements FavoriteDiceRollA
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void setupEditTextAndKeyboard(){
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP) { //Use custom keyboard if the Android version is over 21 (this is when showSoftInputOnFocus was implemented)
+            mCommandInputEditText.setShowSoftInputOnFocus(false);
+
+            mCommandInputConnection = mCommandInputEditText.onCreateInputConnection(new EditorInfo());
+            mDKeyboard.setInputConnection(mCommandInputConnection);
+
+            mCommandInputEditText.setOnFocusChangeListener(new View.OnFocusChangeListener() { //FocusChange listener to show custom keyboard, and minimize keyboard when clicking outside of EditText
+                @Override
+                public void onFocusChange(View v, boolean hasFocus) {
+                    if (hasFocus) {
+                        showCustomKeyboard();
+                    } else {
+                        hideCustomKeyboard();
+                    }
+                }
+            });
+        } else { //Use basic system keyboard
+            mCommandInputEditText.setOnFocusChangeListener(new View.OnFocusChangeListener() { //FocusChange listener to minimize keyboard when clicking outside of EditText
+                @Override
+                public void onFocusChange(View v, boolean hasFocus) {
+                    if (!hasFocus) {
+                        hideSystemKeyboard(v);
+                    }
+                }
+            });
         }
     }
 }
