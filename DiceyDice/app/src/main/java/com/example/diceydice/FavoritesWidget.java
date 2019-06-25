7b9b7a6e029f638fb8785bb2669914e439472fc1
bqfix <benjaminfix@gmail.com>
2019-06-25 15:29:43 -0400

    chore: Altered widget update methods
    Update period set to 0 to prevent timed updates, updating should be achieved through manually calling the updateAppWidget method.

--- DiceyDice/app/src/main/java/com/example/diceydice/FavoritesWidget.java
+++ DiceyDice/app/src/main/java/com/example/diceydice/FavoritesWidget.java
@@ -5,34 +5,39 @@ import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.widget.RemoteViews;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Implementation of App Widget functionality.
  */
 public class FavoritesWidget extends AppWidgetProvider {
 
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
-                                int appWidgetId) {
-
-        CharSequence widgetText = context.getString(R.string.appwidget_text);
-        // Construct the RemoteViews object
-        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.favorites_widget);
-        views.setTextViewText(R.id.appwidget_text, widgetText);
-
-        // Instruct the widget manager to update the widget
-        appWidgetManager.updateAppWidget(appWidgetId, views);
-    }
-
-    @Override
-    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        // There may be multiple widgets active, so update all of them
+                                int[] appWidgetIds, List<DiceRoll> diceRolls) {
+        //Iterate through all widgets and update each
         for (int appWidgetId : appWidgetIds) {
-            updateAppWidget(context, appWidgetManager, appWidgetId);
+            // Construct the RemoteViews object
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.favorites_widget);
+
+            //TODO Add update logic
+
+            // Instruct the widget manager to update the widget
+            appWidgetManager.updateAppWidget(appWidgetId, views);
         }
     }
 
     @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        //This diceRolls is blank, because the onUpdate method should never be called.  Rather, updates occur from the above updateAppWidget method being called by changes in the app.
+        List<DiceRoll> diceRolls = new ArrayList<>();
+        updateAppWidget(context, appWidgetManager, appWidgetIds, diceRolls);
+
+    }
+
+    @Override
     public void onEnabled(Context context) {
-        
+
     }
 
     @Override
